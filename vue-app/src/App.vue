89e902ad1f4f2b153d6e7a4fc2704ea6ac27f8aa
enwumeh <54593478+enwumeh@users.noreply.github.com>
2021-10-02 16:28:04 -0500

    base app set up

---
+++ vue-app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+<div id="app">
+  <main>
+    <div class="search-box">
+      <input type="text" class="search-bar"/>
+      </div>
+  </main>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data: () => {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+    margin: 0;
+}
+</style>
